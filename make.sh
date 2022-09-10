78804d36a5a339b18877b99f5843f9e4ddc7ad20
bob16795 <prestosilver.ptp@gmail.com>
2022-09-10 10:04:21 -0400

    mode gameboy && stdlib stuff

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ ! -f ./slim ]] && ./old/slim.py -o slim ./slim.slm && rm output.*
+[[ ! -f ./slim ]] && ./old/slim.py -o slim ./slim.slm ##&& rm output.*
 
 ./slim -i lib/stdlib.slm,lib/mem.slm,lib/tables.slm,lib/arrays.slm,lib/log.slm,lib/os.slm -o slim slim.slm $@ || echo $?
 
--- makegb.sh
+++ makegb.sh
@@ -4,7 +4,11 @@
 
 ./slim -i lib/stdlib.slm,lib/mem.slm,lib/tables.slm,lib/arrays.slm,lib/log.slm,lib/os.slm -o slim-gb slim-gb.slm
 
+sudo cp slim-gb /usr/bin
+
 rgbgfx -o game/lol.2bpp game/lol.png
+
+nim r game/map.nim game/map.tmx game/map.bin
 
 ./slim-gb -i lib/gblib.slm -o gb.gb game/gb.slm $@ || echo $?
 
