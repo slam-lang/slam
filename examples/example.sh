37051fe50132e5fc1c85d087355bc742f699b56a
bob16795 <prestosilver.ptp@gmail.com>
2022-09-16 07:48:32 -0400

    fix more tests, ccall work

--- examples/example.sh
+++ examples/example.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-../slim -i ../lib -o $1 $1.slm
+../slim -i ../lib -o $1 $1.slm -a
 
 ./$1
 
-rm $1
+#rm $1
--- tests/test.sh
+++ tests/test.sh
@@ -6,4 +6,5 @@ rm -f ./test${1} ./temp.fasm
 
 ../slim  -o test${1} -i ../lib test${1}.slm
 time ./test${1}
-rm ./test${1}
+#rm ./test${1}
+
